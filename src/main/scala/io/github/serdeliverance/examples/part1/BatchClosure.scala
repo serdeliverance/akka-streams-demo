366a148f0f25bf86b8bb1ddcfb6be5a3bd433e26
Sergio Cano <canosergio90@gmail.com>
2021-07-24 22:43:14 -0300

    finished example

--- src/main/scala/io/github/serdeliverance/examples/part1/BatchClosure.scala
+++ src/main/scala/io/github/serdeliverance/examples/part1/BatchClosure.scala
@@ -13,13 +13,32 @@ import java.nio.file.Paths
 object BatchClosure extends App {
 
   implicit val system  = ActorSystem("BatchClosure")
+  implicit val ec      = system.dispatcher
   implicit val session = SlickSession.forConfig("slick-postgres")
 
-  val outputFile = Paths.get("output.txt")
+  private val EMPTY_CARD_TYPE = ""
 
-  Slick
-    .source(transactionTable.filter(t => t.email === "conception.hessel@gmail.com").result)
-    .fold(BigDecimal(0))((acc, tx) => tx.amount + acc)
-    .map(amount => ByteString(amount.toString()))
-    .runWith(FileIO.toPath(outputFile))
+//  val outputFile = Paths.get("client-balance.txt")
+
+//  val result = Slick
+//    .source(transactionTable.filter(t => t.email === "conception.hessel@gmail.com").result)
+//    .fold(BigDecimal(0))((acc, tx) => tx.amount + acc)
+//    .map(amount => ByteString(amount.toString()))
+//    .runWith(FileIO.toPath(outputFile))
+
+  val outputFile2 = Paths.get("brand-balance.txt")
+
+  val result = Slick
+    .source(transactionTable.result)
+    .groupBy(20, _.cardType)
+    .fold((EMPTY_CARD_TYPE, BigDecimal(0)))((acc, tx) => (tx.cardType, tx.amount + acc._2))
+    .mergeSubstreams
+    .map(cardTypeAndTotal => s"cardType: ${cardTypeAndTotal._1}, total: ${cardTypeAndTotal._2}")
+    .map(line => ByteString(line + System.lineSeparator()))
+    .runWith(FileIO.toPath(outputFile2))
+
+  result.onComplete { _ =>
+    system.terminate()
+    session.close()
+  }
 }
