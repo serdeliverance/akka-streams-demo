c9681a1e5264983034d9b8fe56dd9b8b54fffce5
Sergio Cano <canosergio90@gmail.com>
2021-08-05 22:22:22 -0300

    ble

--- src/main/scala/io/github/serdeliverance/utils/EncryptionUtils.scala
+++ src/main/scala/io/github/serdeliverance/utils/EncryptionUtils.scala
@@ -1,5 +1,7 @@
 package io.github.serdeliverance.utils
 
+import com.typesafe.config.ConfigFactory
+
 import java.security.MessageDigest
 import java.util
 import java.util.Base64
@@ -8,6 +10,9 @@ import javax.crypto.spec.SecretKeySpec
 
 trait EncryptionUtils {
 
+  private lazy val config = ConfigFactory.load()
+  private lazy val salt   = config.getString("encryption.salt")
+
   def encrypt(key: String, value: String): String = {
     val cipher: Cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
     cipher.init(Cipher.ENCRYPT_MODE, keyToSpec(key))
@@ -15,13 +20,10 @@ trait EncryptionUtils {
   }
 
   def keyToSpec(key: String): SecretKeySpec = {
-    var keyBytes: Array[Byte] = (SALT + key).getBytes("UTF-8")
+    var keyBytes: Array[Byte] = (salt + key).getBytes("UTF-8")
     val sha: MessageDigest    = MessageDigest.getInstance("SHA-1")
     keyBytes = sha.digest(keyBytes)
     keyBytes = util.Arrays.copyOf(keyBytes, 16)
     new SecretKeySpec(keyBytes, "AES")
   }
-
-  private val SALT: String =
-    "jMhKlOuJnM34G6NHkqo9V010GhLAqOpF0BePojHgh1HgNg8^72k"
 }
